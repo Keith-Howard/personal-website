f1eafd6480d7deb72c7f70b42ed70447828c84eb
keith-howard <keithwhoward88@gmail.com>
2023-01-05 00:03:18 -0500

    pictures of 8 balls

--- src/components/Magic8Ball.js
+++ src/components/Magic8Ball.js
@@ -1,11 +1,22 @@
 import './Magic8Ball.css';
+import React from 'react';
+
 function Magic8Ball() {
-    console.log('Test');
+    const [eightBall, setEightBall] = React.useState("./magic8BallPics/magic8ball.png");
+    const responses = [["absolutely", "countOnIt", "no", "notNow", "veryLikely", "yes"],
+                         ["askAgain", "cannotTellNow", "maybe", "waitForIt"]];
     return (
         <div className="eightballBackground">
             <div className='magic8BallContainer'>
-                <input placeholder='ASK QUESTION...'></input>
-                <button>Shake</button>
+                <table className='eightBalluiTable'>
+                    <tr>
+                        <img className="eightballImg" src={eightBall}></img>
+                    </tr>
+                    <tr>
+                        <input className="inputQuestion" placeholder='ASK QUESTION...'></input>
+                        <button onClick={()=> setEightBall("./magic8BallPics/yes.png")}>Shake!</button>
+                    </tr>
+                </table>
             </div>  
         </div>
     )
