2d3b51279d76f507aa505eb09343e91af90f3303
keith-howard <keithwhoward88@gmail.com>
2023-01-04 21:38:37 -0500

    update 8 ball

--- src/components/Magic8Ball.js
+++ src/components/Magic8Ball.js
@@ -2,9 +2,11 @@ import './Magic8Ball.css';
 function Magic8Ball() {
     console.log('Test');
     return (
-        <div className="magic8BallContainer">
-            <input placeholder='ASK QUESTION'></input>
-            <button>Shake</button>
+        <div className="eightballBackground">
+            <div className='magic8BallContainer'>
+                <input placeholder='ASK QUESTION...'></input>
+                <button>Shake</button>
+            </div>  
         </div>
     )
 }
