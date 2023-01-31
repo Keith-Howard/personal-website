5c083ad4049a13d5b34feaea5f44da3ccfddf4d4
keith-howard <keithwhoward88@gmail.com>
2023-01-31 17:58:41 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Routes, Route, HashRouter} from 'react-router-dom';
 import Home from './components/Home';
 import RockPaperScissors from './components/RockPaperScissors';
 import Magic8Ball from './components/Magic8Ball';
+import MyArt from './components/MyArt';
 import NavBar from './components/Navbar';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <div className="navbarContainer ml-2">
             <Routes>
               <Route path="/components/Magic8Ball" element={<Magic8Ball />} />
+              <Route path="/components/MyArt" element={<MyArt />} />
               <Route path="/components/RockPaperScissors" element={<RockPaperScissors />} />
               <Route path="/" element={<Home />} />
             </Routes>
