5a599917c928440e7a0c2934bd4606b5df7847dc
keith-howard <keithwhoward88@gmail.com>
2023-02-01 08:34:48 -0500

    updated background

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,17 @@ import './Home.css';
 function Home() {
     console.log('Home');
     return (
-        <>
-        <h1 className="homeText">Welcome</h1>
-        <h1 className="homeText">To My Site,</h1>
-        <h1 className="homeText">Browse My Art</h1>
-        <h1 className="homeText">Or</h1>
-        <h1 className="homeText">Play Rock Paper Scissors</h1>
-        <h1 className="homeText">Lookout for Future Updates</h1>
-        <h1 className="homeText">Enjoy</h1>
-        </>
+        <div className="homeWrapper">
+            <div className="homeContainer">
+                <h1 className="homeText">Welcome</h1>
+                <h1 className="homeText">To My Site,</h1>
+                <h1 className="homeText">Browse My Art</h1>
+                <h1 className="homeText">Or</h1>
+                <h1 className="homeText">Play Rock Paper Scissors,</h1>
+                <h1 className="homeText">Lookout for Future Updates</h1>
+                <h1 className="homeText">Enjoy</h1>
+            </div>
+        </div>
     )
 }
 
